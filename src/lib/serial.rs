0300547ef1f66968f3f44bab43e08aaf626e63c2
mitchmindtree <mitchell.nordine@gmail.com>
2019-08-06 01:50:51 +0200

    Rewrite serial data processing after testing with device
    Closes #2

--- src/lib/serial.rs
+++ src/lib/serial.rs
@@ -7,10 +7,9 @@ use std::cell::RefCell;
 use std::sync::atomic::{self, AtomicBool};
 use std::sync::{mpsc, Arc};
 
-const BAUD_RATE: u32 = 3_000_000;
-const BUFFER_LEN: usize = 41;
-const DATA_PER_BUFFER: u8 = 40;
-const NON_DATA_BUFFER_N: u8 = 51;
+const BAUD_RATE: u32 = 1_500_000;
+const DATA_PER_BUFFER: u32 = 40;
+const TOTAL_BUFFERS_PER_FRAME: u32 = 51;
 
 /// A handle to the receiving serial thread.
 pub struct Handle {
@@ -21,19 +20,41 @@ pub struct Handle {
     port_info: SerialPortInfo,
 }
 
+enum State {
+    CountingZeros,
+    InSync,
+}
+
+struct ReceiverContext {
+    rx_buffer: [u8; 256],
+    rx_buffer_index: u32,
+    rx_buffer_count: u32,
+    state: State,
+    bufnum: u32,
+    count: u32,
+    buffer: [u8; 40],
+    screen_buffer: Box<vis::Cbm8032FrameData>,
+    graphic: vis::Cbm8032FrameMode,
+}
+
+fn init_receiver_context() -> ReceiverContext {
+    ReceiverContext {
+        rx_buffer: [0u8; 256],
+        rx_buffer_index: 0,
+        rx_buffer_count: 0,
+        bufnum: 0,
+        count: 0,
+        state: State::CountingZeros,
+        buffer: [0u8; 40],
+        screen_buffer: Box::new([0u8; 2000]),
+        graphic: vis::Cbm8032FrameMode::Graphics,
+    }
+}
+
 type Message = (vis::Cbm8032Frame, FrameHz);
 type ChannelRx = mpsc::Receiver<Message>;
 type ChannelTx = mpsc::Sender<Message>;
 type SerialPortObj = dyn SerialPort;
-
-enum State {
-    CountingZeroes(u8),
-    InSync {
-        last_buffer_n: u8,
-        buffer: Vec<u8>,
-        vis_frame_data: Vec<u8>,
-    },
-}
 
 /// The rate at which the serial stream is producing frames.
 #[derive(Clone, Copy, Default)]
@@ -87,7 +108,7 @@ fn find_usb_port() -> Result<Option<SerialPortInfo>, serialport::Error> {
 fn open_port(name: &str) -> Result<Box<SerialPortObj>, serialport::Error> {
     let mut settings = SerialPortSettings::default();
     settings.baud_rate = BAUD_RATE.into();
-    settings.timeout = std::time::Duration::from_millis(17);
+    settings.timeout = std::time::Duration::from_secs(1);
     serialport::open_with_settings(&name, &settings)
 }
 
@@ -95,7 +116,11 @@ fn open_port(name: &str) -> Result<Box<SerialPortObj>, serialport::Error> {
 fn read_from(port: &mut Box<SerialPortObj>, buffer: &mut [u8]) -> usize {
     match port.read(buffer) {
         Err(e) => match e.kind() {
-            std::io::ErrorKind::TimedOut | std::io::ErrorKind::WouldBlock => 0,
+            std::io::ErrorKind::TimedOut => {
+                eprintln!("no serial data received in the last second");
+                0
+            }
+            std::io::ErrorKind::WouldBlock => 0,
             _ => {
                 eprintln!(
                     "An error occurred while reading from the serial port: {}",
@@ -108,92 +133,114 @@ fn read_from(port: &mut Box<SerialPortObj>, buffer: &mut [u8]) -> usize {
     }
 }
 
-// Open the serial port and run the read loop.
-fn run(mut port: Box<SerialPortObj>, vis_frame_tx: ChannelTx, is_closed: Arc<AtomicBool>) {
-    let mut state = State::CountingZeroes(0);
-    let mut buffer = vec![0u8; 1_000];
-    let fps = Fps::default();
-    while !is_closed.load(atomic::Ordering::Relaxed) {
-        let len = read_from(&mut port, &mut buffer[..]);
-        let mut slice = &buffer[..len];
-        while !slice.is_empty() {
-            match state {
-                // If we're counting zeroes, check for another.
-                State::CountingZeroes(ref mut n_zeroes) => {
-                    match slice[0] {
-                        0 => *n_zeroes += 1,
-                        _ => *n_zeroes = 0,
-                    }
-                    slice = &slice[1..];
-                    if *n_zeroes == DATA_PER_BUFFER {
-                        state = State::InSync {
-                            last_buffer_n: 0,
-                            buffer: Vec::with_capacity(BUFFER_LEN),
-                            vis_frame_data: Vec::with_capacity(vis::CBM_8032_FRAME_DATA_LEN),
-                        };
-                    }
+fn read_bytes(port: &mut Box<SerialPortObj>, buffer: &mut [u8]) -> usize {
+    read_from(port, buffer)
+}
+
+fn byte_to_mode(byte: u8) -> vis::Cbm8032FrameMode {
+    match byte {
+        0 => vis::Cbm8032FrameMode::Graphics,
+        _ => vis::Cbm8032FrameMode::Text,
+    }
+}
+
+fn handle_received_buffer(context: &mut ReceiverContext) {
+    if context.bufnum > 0 {
+        if context.bufnum < TOTAL_BUFFERS_PER_FRAME {
+            let bufidx = context.bufnum - 1;
+            let screen_start = (bufidx * DATA_PER_BUFFER) as usize;
+            let screen_end = screen_start + DATA_PER_BUFFER as usize;
+            let screen_slice = &mut context.screen_buffer[screen_start..screen_end];
+            screen_slice.copy_from_slice(&context.buffer);
+        } else {
+            context.graphic = byte_to_mode(context.buffer[0]);
+        }
+    }
+}
+
+fn handle_sync_loss(context: &ReceiverContext, byte: u8) {
+    eprintln!(
+        "out of sync at bufnum {} count {} - received {}\n",
+        context.bufnum, context.count, byte
+    );
+}
+
+fn handle_received_byte(context: &mut ReceiverContext, byte: u8) -> bool {
+    let mut screen_complete = false;
+    match context.state {
+        State::CountingZeros => {
+            if byte == 0 {
+                context.count += 1;
+                if context.count == 41 {
+                    context.state = State::InSync;
+                    context.bufnum = 1;
+                    context.count = 0;
                 }
-
-                // If we're synchronised, read until the next full buffer.
-                State::InSync {
-                    ref mut last_buffer_n,
-                    ref mut buffer,
-                    ref mut vis_frame_data,
-                } => {
-                    if buffer.len() < BUFFER_LEN {
-                        let remaining = BUFFER_LEN - buffer.len();
-                        let end = std::cmp::min(remaining, slice.len());
-                        buffer.extend(slice[..end].iter().cloned());
-                        slice = &slice[end..];
-                        continue;
+            } else {
+                context.count = 0;
+            }
+        }
+        State::InSync => {
+            if context.count < 40 {
+                context.buffer[context.count as usize] = byte;
+                context.count += 1;
+            } else {
+                if byte == context.bufnum as u8 {
+                    handle_received_buffer(context);
+                    context.bufnum += 1;
+                    context.count = 0;
+                    if context.bufnum == 52 {
+                        context.state = State::CountingZeros;
+                        screen_complete = true;
                     }
-
-                    // If this is the 0th buffer, skip it.
-                    let buffer_n = slice[0];
-                    slice = &slice[1..];
-                    if buffer_n == NON_DATA_BUFFER_N {
-                        *last_buffer_n = buffer_n;
-                        // If the last buffer was 51, this is the zeroed sync frame.
-                        if *last_buffer_n == NON_DATA_BUFFER_N {
-                            *last_buffer_n = 0;
-                        // Otherwise this frame contains the graphics mode. The frame is ready.
-                        } else {
-                            let mode = match buffer[0] {
-                                0 => vis::Cbm8032FrameMode::Text,
-                                _ => vis::Cbm8032FrameMode::Graphics,
-                            };
-                            let mut data = Box::new([0u8; vis::CBM_8032_FRAME_DATA_LEN]);
-                            data.copy_from_slice(vis_frame_data);
-                            buffer.clear();
-                            let frame = vis::Cbm8032Frame::new(mode, data);
-
-                            // Sample the rate at which serial data is producing frames.
-                            fps.sample();
-                            let avg = fps.avg();
-                            let min = fps.min();
-                            let max = fps.max();
-                            let hz = FrameHz { avg, min, max };
-
-                            if vis_frame_tx.send((frame, hz)).is_err() {
-                                eprintln!("lost connecton to main thread, closing serial thread");
-                                return;
-                            }
-                        }
-                        continue;
-                    }
-
-                    // Check we're still synchronised.
-                    if buffer_n - 1 != *last_buffer_n {
-                        eprintln!("lost serial sync, attempting re-sync");
-                        state = State::CountingZeroes(0);
-                        continue;
-                    } else {
-                        *last_buffer_n = buffer_n;
-                    }
-
-                    vis_frame_data.extend(buffer.drain(..));
+                } else {
+                    handle_sync_loss(context, byte);
+                    context.state = State::CountingZeros;
+                    context.count = 0;
                 }
             }
+        }
+    }
+    screen_complete
+}
+
+fn receive_screen(port: &mut Box<SerialPortObj>, context: &mut ReceiverContext) {
+    loop {
+        if context.rx_buffer_index == context.rx_buffer_count {
+            context.rx_buffer_index = 0;
+            context.rx_buffer_count = read_bytes(port, &mut context.rx_buffer) as _;
+        } else {
+            let ix = context.rx_buffer_index;
+            context.rx_buffer_index += 1;
+            let received_byte = context.rx_buffer[ix as usize];
+            if handle_received_byte(context, received_byte) {
+                return;
+            }
+        }
+    }
+}
+
+// Open the serial port and run the read loop.
+fn run(mut port: Box<SerialPortObj>, vis_frame_tx: ChannelTx, is_closed: Arc<AtomicBool>) {
+    let fps = Fps::default();
+    let mut context = init_receiver_context();
+    while !is_closed.load(atomic::Ordering::Relaxed) {
+        receive_screen(&mut port, &mut context);
+
+        // Construct the frame.
+        let frame = vis::Cbm8032Frame::new(context.graphic, context.screen_buffer.clone());
+
+        // Sample the rate at which serial data is producing frames.
+        fps.sample();
+        let avg = fps.avg();
+        let min = fps.min();
+        let max = fps.max();
+        let hz = FrameHz { avg, min, max };
+
+        // Send the frame to the main thread.
+        if vis_frame_tx.send((frame, hz)).is_err() {
+            eprintln!("lost connecton to main thread, closing serial thread");
+            return;
         }
     }
 }
@@ -214,9 +261,7 @@ pub fn spawn() -> Result<Handle, serialport::Error> {
     let port = open_port(&info.port_name)?;
     let thread = std::thread::Builder::new()
         .name("serial_rx_thread".into())
-        .spawn(move || {
-            run(port, tx, is_closed2)
-        })
+        .spawn(move || run(port, tx, is_closed2))
         .expect("failed to spawn serial rx thread");
     let last_recorded_frame_hz = RefCell::new(FrameHz::default());
     Ok(Handle {
